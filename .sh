10744a46265dc643c950b3f0f76e308fc9b8d383
fengjinliang <fengjinliang@streamhub.tech>
2020-04-24 17:19:08 +0800

    Signed-off-by: fengjinliang <fengjinliang@streamhub.tech>

--- .sh
+++ .sh
@@ -3,6 +3,7 @@ echo $(date "+%Y - %m - %d %H : %M : %S") >> /fff/log/time_begin.log
 
 chmod -R 777 /fff/html
 cd /fff/html/swoole
+git checkout .
 git pull origin master
 
 echo $(date "+%Y - %m - %d %H : %M : %S") >> /fff/log/time_end.log
