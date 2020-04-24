6a5ace95de219b342d22dda30e1dc13d0575a2b2
fengjinliang <fengjinliang@streamhub.tech>
2020-04-24 17:14:30 +0800

    Signed-off-by: fengjinliang <fengjinliang@streamhub.tech>

--- .sh
+++ .sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo $(date "+%Y%m%d%H%M%S") >> /fff/log/time_begin.log
+echo $(date "+%Y - %m - %d %H : %M : %S") >> /fff/log/time_begin.log
 
 chmod -R 777 /fff/html
 cd /fff/html/swoole
 git pull origin master
 
-echo $(date "+%Y%m%d%H%M%S") >> /fff/log/time_end.log
+echo $(date "+%Y - %m - %d %H : %M : %S") >> /fff/log/time_end.log
 
 php /fff/html/swoole/http_server.php
 
-echo $(date "+%Y%m%d%H%M%S") >> /fff/log/time_php.log
+echo $(date "+%Y - %m - %d %H : %M : %S") >> /fff/log/time_php.log
