0d006ca5fdc9b85c978cea2890339214214b3a54
fengjinliang <fengjinliang@streamhub.tech>
2020-04-24 17:46:07 +0800

    Signed-off-by: fengjinliang <fengjinliang@streamhub.tech>

--- http_server.php
+++ http_server.php
@@ -4,10 +4,10 @@ $http = new Swoole\Http\Server("0.0.0.0", 32773);
 
 $http->on('request', function ($request, $response) {
     //避免chrome请求两次
-    if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
-        $response->end();
-        return;
-    }
+//    if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
+//        $response->end();
+//        return;
+//    }
     var_dump($request->get, $request->post);
     $response->header("Content-Type", "text/html; charset=utf-8");
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
